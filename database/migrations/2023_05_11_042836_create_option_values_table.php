410a95841c269af1bb89447f3fba87cfee46c6c9
thanhd <thanhdanh@icd-vn.com>
2023-05-19 14:09:32 +0700

    update: update option_value migration

--- database/migrations/2023_05_11_042836_create_option_values_table.php
+++ database/migrations/2023_05_11_042836_create_option_values_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Option;
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('option_values', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Product::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(\App\Models\Option::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Option::class)->constrained()->cascadeOnDelete();
             $table->string('value');
             $table->string('label')->nullable();
             $table->timestamps();
