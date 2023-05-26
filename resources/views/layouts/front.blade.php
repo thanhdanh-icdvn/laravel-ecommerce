48203b3f155cdde0ca5b8f9ebfdb13158e82ad96
thanhd <thanhdanh@icd-vn.com>
2023-05-27 03:07:21 +0700

    fix: fix error when run sail

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,7 +20,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/css/front.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased text-gray-900 ">
