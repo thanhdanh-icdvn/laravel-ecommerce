7d8a24faca5439cb311e87fb3db3b686974821d2
thanhd <thanhdanh@icd-vn.com>
2023-06-20 21:18:49 +0700

    update: fix composer

--- resources/views/front/top/index.blade.php
+++ resources/views/front/top/index.blade.php
@@ -1,19 +1,28 @@
 <x-front-layout>
     <x-slot name="title">{{ __('Trang chủ') }}</x-slot>
     <div class="w-full overflow-x-hidden bg-slate-200 pt-[30px] pb-[60px]">
-        <div class="banner-wrapper w-full">
+        <div class="w-full banner-wrapper">
             <div class="container mx-auto">
-                <div class="main-wrapper w-full">
-                    <div class="banner-card">
-                        <div data-aos="fade-right" class="h-full w-full xl:w-1/2">
-                            <img src="{{ Vite::image('banners/bnr_001.png') }}" alt=""
-                                class="h-auto w-full max-w-full object-cover">
+                <div class="flex main-wrapper">
+                    <div class="banner-card xl:flex xl:space-x-[30px]  mb-[30px]">
+                        <div data-aos="fade-right" class="w-full h-full xl:w-1/2">
+                            <a href="">
+                                <picture>
+                                    <source media="(min-width: 1025px)" srcset="{{ Vite::image('banners/bnr_001.png') }}">
+                                    <img src="{{ Vite::image('banners/bnr_002.png') }}" alt=""
+                                        class="object-cover w-full h-auto max-w-full">
+                                </picture>
+                            </a>
                         </div>
                     </div>
-                    <div class="banner-card">
+                    <div class="banner-card xl:flex xl:space-x-[30px]  mb-[30px]">
                         <div data-aos="fade-left" class="flex h-full w-1/2 flex-row xl:flex-col xl:space-y-[30px]">
-                            <img src="{{ Vite::image('banners/bnr_002.png') }}" alt=""
-                                class="h-auto w-full max-w-full object-cover">
+                            <div class="w-full">
+                                <a href="">
+                                    <img src="{{ Vite::image('banners/bnr_003.png') }}" alt=""
+                                        class="object-cover w-full h-auto max-w-full">
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -22,14 +31,14 @@
     </div>
     <div class="market-category mb-[60px]">
         <div class="container mx-auto">
-            <div class="justify-betweeen section-title mb-5 flex items-center">
+            <div class="flex items-center justify-between mb-5 section-title">
                 <h2 class="text-3xl font-bold leading-tight">My Market Category</h2>
                 <div>
                     <a href="/view-more" class="text-lg font-bold">
                         View more
                         <span class="">
                             <img src="{{ Vite::image('arrow-viewmore.svg') }}" alt=""
-                                class="inline-block h-4 w-4">
+                                class="inline-block w-4 h-4">
                         </span>
                     </a>
                 </div>
